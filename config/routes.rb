af86823315c494fe9b852e2f1fae5612d370d3c9
Charles Arciniega <charlesarcin@gmail.com>
2014-08-18 10:56:48 -0700

    root page to index and create custom route for /allcategories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :categories, :soups
+  root to: "categories#index"
+  get "/allcategories", to: redirect('/')
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
