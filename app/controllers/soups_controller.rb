e84b6644e797e5aed0f533cfb788da7d2c0fd5ca
Charles Arciniega <charlesarcin@gmail.com>
2014-08-17 21:12:11 -0700

    Soup show page now responds to html and json

--- app/controllers/soups_controller.rb
+++ app/controllers/soups_controller.rb
@@ -1,5 +1,10 @@
 class SoupsController < ApplicationController
   def show
     @soup = Soup.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @soup }
+    end
   end
 end
